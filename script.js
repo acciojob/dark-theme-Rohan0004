b6a0afdb5063674db28539b32bd4436f37f08d8d
Rohan0004 <110601451+Rohan0004@users.noreply.github.com>
2023-09-09 21:12:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
-
+let cnt=0;
+let swapTheme = function() {
+	let color=document.getElementById("app");
+	if (cnt%2==0) {
+		color.classList.add("night");
+	}else{
+		color.classList.remove("night");
+	}
+	cnt++;
+}
